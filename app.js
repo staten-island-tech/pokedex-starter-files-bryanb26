88a9471d5ee8cfee49454a6a0c276f9fea984b07
bryanb26 <50746711+bryanb26@users.noreply.github.com>
2019-10-24 11:47:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DOMStrings = {
+/* const DOMStrings = {
     input: document.getElementById('pkmn-form'),
     name: document.getElementById('pokemon-name'),
     displayName: document.querySelector('.pkmn-name-size'),
@@ -30,3 +30,50 @@ function getPkmn() {
     })
 };
 getPkmn();
+ */
+const DOMStrings = {
+    input: document.getElementById("pkmn-form"),
+    name: document.getElementById("pokemon-name"),
+    displayName: document.querySelector(".pkmn-name-size"),
+    displayImageFront: document.querySelector(".display-image-front-def"),
+    displayImageBack: document.querySelector(".display-image-back-def"),
+    displayImageShinyFront: document.querySelector(".display-image-shiny-front"),
+    displayImageShinyBack: document.querySelector(".display-image-shiny-back"),
+    type: document.querySelector(".type"),
+    displayNum: document.querySelector(".pkmn-num")
+    };
+    
+    DOMStrings.input.addEventListener("keypress", function(event){
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.querySelector(".nes-btn").click();
+        }
+    })
+    function getPkmn() {
+    DOMStrings.input.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    try {
+    const result = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${DOMStrings.name.value}`
+    );
+    const data = await result.json();
+    //console.log(data);
+    
+    const displayPkmn = function(data) {
+        DOMStrings.displayName.innerText = data.name;
+        DOMStrings.displayNum.innerText = data.id;
+        DOMStrings.displayImageFront.src = data.sprites.front_default;
+        DOMStrings.displayImageBack.src = data.sprites.back_default;
+        DOMStrings.displayImageShinyFront.src = data.sprites.front_shiny;
+        DOMStrings.displayImageShinyBack.src = data.sprites.back_shiny;
+        DOMStrings.type.textContent = data.types.map(data => data.type.name);
+
+    };
+    displayPkmn(data);
+    DOMStrings.name.value = "";
+    } catch (err) {
+    console.log(err);
+    }
+    });
+    }
+    getPkmn();
